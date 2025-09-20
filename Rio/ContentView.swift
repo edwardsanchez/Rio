158c214b7a337cdc5396d5966cac16b22b09266f
Edward Sanchez <edwardsanchez@mac.com>
2025-09-19 23:53:00 -0700

    Chat Bubbles

--- Rio/ContentView.swift
+++ Rio/ContentView.swift
@@ -13,8 +13,64 @@ struct ContentView: View {
     @Query private var items: [Item]
 
     var body: some View {
-        VStack {
-            Text("Hi")
+        ScrollView {
+            VStack(spacing: 20) {
+                HStack(alignment: .bottom) {
+                    Image(.usersample)
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .clipShape(.circle)
+                        .offset(y: 10)
+                    Text("Hi Rio!\nHow are you doing today?")
+                        .foregroundStyle(.primary)
+                        .padding()
+                        .background{
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color.userBubble)
+                                .overlay(alignment: .bottomLeading) {
+                                    Image(.cartouche)
+                                        .resizable()
+                                        .frame(width: 15, height: 15)
+                                        .rotation3DEffect(Angle(degrees: 180), axis: (x: 0, y: 1, z: 0))
+                                        .offset(x: 5, y: 5.5)
+                                        .foregroundStyle(Color.userBubble)
+                                }
+                                .compositingGroup()
+                                .opacity(0.6)
+                        }
+                    Spacer()
+                }
+                HStack() {
+                    Spacer()
+                    Text("Hey!\nI'm doing well, thanks for asking!")
+                        .foregroundStyle(.white)
+                        .padding()
+                        .background {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color.ownBubble)
+                                .overlay(alignment: .bottomTrailing) {
+                                    Image(.cartouche)
+                                        .resizable()
+                                        .frame(width: 15, height: 15)
+                                        .offset(x: -5, y: 5.5)
+                                        .foregroundStyle(Color.ownBubble)
+                                }
+                        }
+                }
+            }
+        }
+        .contentMargins(20)
+        .frame(maxHeight: .infinity)
+        .background {
+            Color.base
+                .ignoresSafeArea()
+        }
+        .overlay {
+            Rectangle()
+                .fill(Gradient(colors: [.white, .black]))
+                .ignoresSafeArea()
+                .opacity(0.2)
+                .blendMode(.overlay)
         }
     }
 }
