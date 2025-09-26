6d674c3ab6f49b31ef104a4f613e7f76d0cf0512
Edward Sanchez <edwardsanchez@mac.com>
2025-09-25 23:45:36 -0700

    Remove logging for letters

--- Rio/CursiveLetter.swift
+++ Rio/CursiveLetter.swift
@@ -298,13 +298,13 @@ struct CursiveWordShape: Shape {
             }
         }
 
-        let finalBounds = positioned.boundingBox
-        let inside = rect.contains(finalBounds)
-        let overflowTop = rect.minY - finalBounds.minY
-        let overflowLeft = rect.minX - finalBounds.minX
-        let overflowBottom = finalBounds.maxY - rect.maxY
-        let overflowRight = finalBounds.maxX - rect.maxX
-        print("CursiveWordShape: rect=\(rect) bounds=\(finalBounds) inside=\(inside) baselineY=\(baselineInRect) dy=\(dy). Overflows (top,left,bottom,right)=\(overflowTop),\(overflowLeft),\(overflowBottom),\(overflowRight)")
+//        let finalBounds = positioned.boundingBox
+//        let inside = rect.contains(finalBounds)
+//        let overflowTop = rect.minY - finalBounds.minY
+//        let overflowLeft = rect.minX - finalBounds.minX
+//        let overflowBottom = finalBounds.maxY - rect.maxY
+//        let overflowRight = finalBounds.maxX - rect.maxX
+//        print("CursiveWordShape: rect=\(rect) bounds=\(finalBounds) inside=\(inside) baselineY=\(baselineInRect) dy=\(dy). Overflows (top,left,bottom,right)=\(overflowTop),\(overflowLeft),\(overflowBottom),\(overflowRight)")
 
         return Path(positioned)
     }
