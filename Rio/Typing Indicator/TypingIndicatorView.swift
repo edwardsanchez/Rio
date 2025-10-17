4ae4f11c227498282a8ea66d9861d4e61578e256
Edward Sanchez <edwardsanchez@mac.com>
2025-10-16 18:40:53 -0700

    Fix typing indicator

--- Rio/Typing Indicator/TypingIndicatorView.swift
+++ Rio/Typing Indicator/TypingIndicatorView.swift
@@ -12,39 +12,31 @@ import SwiftUI
 struct TypingIndicatorView: View {
     var isVisible: Bool
     @State private var isAnimating = false
-    @State private var shown = true
     @State private var height: CGFloat = 0
 
     var body: some View {
         ZStack {
-            if shown {
-                HStack(spacing: 5) {
-                    ForEach(0..<3) { index in
-                        Circle()
-                            .fill(Color.primary.opacity(0.4))
-                            .frame(width: 10, height: 10)
-                            .scaleEffect(isAnimating ? 1.0 : 0.5)
-                            .opacity(isAnimating ? 1 : 0.5)
-                            .animation(
-                                .easeInOut(duration: 0.8)
-                                .repeatForever(autoreverses: true)
-                                .delay(Double(index) * 0.3),
-                                value: isAnimating
-                            )
-                            .scaleEffect(isVisible ? 1 : 0)
-                            .opacity(isVisible ? 1 : 0)
-                            .animation(.easeIn(duration: 0.05).delay(CGFloat(index - 1) * 0.1), value: isAnimating)
-                    }
+            HStack(spacing: 5) {
+                ForEach(0..<3) { index in
+                    Circle()
+                        .fill(Color.primary.opacity(0.4))
+                        .frame(width: 10, height: 10)
+                        .scaleEffect(isAnimating ? 1.0 : 0.5)
+                        .opacity(isAnimating ? 1 : 0.5)
+                        .animation(
+                            .easeInOut(duration: 0.8)
+                            .repeatForever(autoreverses: true)
+                            .delay(Double(index) * 0.3),
+                            value: isAnimating
+                        )
+                        .scaleEffect(isVisible ? 1 : 0)
+                        .opacity(isVisible ? 1 : 0)
+                        .animation(.easeIn(duration: 0.05).delay(CGFloat(index - 1) * 0.1), value: isAnimating)
                 }
             }
         }
         .onAppear {
             isAnimating = true
-        }
-        .onChange(of: isVisible) { oldValue, newValue in
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                shown = newValue
-            }
         }
     }
 }
